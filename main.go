5f57c1cb49ccf73dec66b00320b34f3f339a47b2
Enyytime <rogeliokennya@gmail.com>
2025-04-10 15:48:50 +0700

    add check

--- main.go
+++ main.go
@@ -33,8 +33,25 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func checkHandler(w http.ResponseWriter, r *http.Request) {
+	now := time.Now()
+	uptime := int64(now.Sub(startTime).Seconds())
+
+	response := HealthResponse{
+		Nama:      "Rogelio Kenny Arisandi",
+		NRP:       "5025231074",
+		Status:    "UP",
+		Timestamp: now.Unix(),
+		Uptime:    uptime,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
 func main() {
 	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/check", checkHandler)
 	port := ":8080"
 	log.Printf("Server is running on port %s", port)
 	log.Fatal(http.ListenAndServe(port, nil))
